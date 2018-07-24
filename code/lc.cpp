a540a94fa7c3fc58f5d2bcd2cee2825ce913007a
DonutLaser <vidmantas.luneckas@gmail.com>
2018-07-24 20:19:13 +0300

    Added slider handles to easier see what comoponent values are

--- code/lc.cpp
+++ code/lc.cpp
@@ -1,5 +1,6 @@
 #include "lc.h"
 
+#include "lc_platform.h"
 #include "lc_shared.h"
 #include "lc_memory.h"
 #include "lc_opengl.h"
@@ -9,9 +10,11 @@
 #define HORIZONTAL_PADDING 10 
 #define VERTICAL_PADDING 5 
 #define SLIDER_HEIGHT 15 
+#define SLIDER_HANDLE_WIDTH 8
 #define CLEAR_COLOR 200, 200, 200
 #define DEFAULT_COLOR 255, 255, 255 
 #define HIGHLIGHT_COLOR 160, 160, 160
+#define HANDLE_COLOR 220, 220, 220
 
 #define KEY_COMMA 	0xBC
 #define KEY_POINT 	0xBE
@@ -84,13 +87,15 @@ static int auto_layout (int* remaining_free_space_on_y, int rect_height) {
 	return result;
 }
 
-static void draw_slider (int width, int height, lc_color color, int* remaining_y_space, bool is_selected) {
+static void draw_slider (int width, int height, lc_color color, float max_value, float value, int* remaining_y_space, bool is_selected) {
+	// Calculate the slider rect
 	lc_rect rect = { };
 	rect.x = HORIZONTAL_PADDING;
 	rect.y = auto_layout (remaining_y_space, height);
 	rect.width = width;
 	rect.height = height;
 
+	// Draw the highlight if selected
 	if (is_selected) {
 		lc_color highlight_color = make_colorb (HIGHLIGHT_COLOR);
 		lc_rect highlight_rect = { };
@@ -101,16 +106,32 @@ static void draw_slider (int width, int height, lc_color color, int* remaining_y
 		opengl_rect (highlight_rect, highlight_color);
 	}
 
+	// Draw slider
 	opengl_rect (rect, color);
+
+	byte value_in_bytes = (byte)((value * 255.0f) / max_value);
+	byte max_value_in_bytes = (byte)(max_value * 255.0f);
+
+	// Draw slider handle
+	lc_color handle_color = make_colorb (HANDLE_COLOR);
+	lc_rect handle_rect = { };
+	handle_rect.width = SLIDER_HANDLE_WIDTH;
+	handle_rect.height = height;
+	handle_rect.x = rect.x + ((value_in_bytes * (width - handle_rect.width)) / max_value_in_bytes);
+	handle_rect.y = rect.y;
+
+	opengl_rect (handle_rect, handle_color);
 }
 
-void app_init (lc_memory* memory, int client_width, int client_height) {
+void app_init (lc_memory* memory, platform_api platform, int client_width, int client_height) {
 	lc_app* app = (lc_app*)memory -> storage;
 	app -> current_color = make_colorb (DEFAULT_COLOR);
 	app -> current_component = CC_R;
 
 	app -> client_width = client_width;
 	app -> client_height = client_height;
+
+	app -> platform = platform;
 }
 
 void app_update (lc_memory* memory, lc_input input) {
@@ -133,10 +154,10 @@ void app_update (lc_memory* memory, lc_input input) {
 	opengl_rect (color_rect, color);
 
 	int slider_width = app -> client_width - (2 * HORIZONTAL_PADDING);
-	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (255, 0, 0), &remaining_y_space,
-				 app -> current_component == CC_R);
-	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (0, 255, 0), &remaining_y_space, 
-				 app -> current_component == CC_G);
-	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (0, 0, 225), &remaining_y_space, 
-				 app -> current_component == CC_B);
+	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (255, 0, 0), 1.0f, app -> current_color.r,
+				 &remaining_y_space, app -> current_component == CC_R);
+	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (0, 255, 0), 1.0f, app -> current_color.g,
+				 &remaining_y_space, app -> current_component == CC_G);
+	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (0, 0, 225), 1.0f, app -> current_color.b,
+				 &remaining_y_space, app -> current_component == CC_B);
 }
