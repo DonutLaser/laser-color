48855f001b0d61a4cb3c1dc84449a07de904a07f
DonutLaser <vidmantas.luneckas@gmail.com>
2018-07-25 22:30:28 +0300

    Drawing the swatches + auto layouting improvements

--- code/lc.cpp
+++ code/lc.cpp
@@ -7,6 +7,7 @@
 #include "lc_shared.h"
 #include "lc_memory.h"
 #include "lc_opengl.h"
+#include "lc_gui_layout.h"
 
 #define clamp_value(x, min, max) (x < min) ? min : ((x > max) ? max : x)
 
@@ -14,6 +15,8 @@
 #define VERTICAL_PADDING 5 
 #define SLIDER_HEIGHT 15 
 #define SLIDER_HANDLE_WIDTH 8
+#define SWATCH_HEIGHT 15
+#define SWATCH_WIDTH 15
 #define CLEAR_COLOR 200, 200, 200
 #define DEFAULT_COLOR 255, 255, 255 
 #define HIGHLIGHT_COLOR 160, 160, 160
@@ -127,25 +130,18 @@ static void handle_input (lc_app* app, lc_input input) {
 	}
 }
 
-static int auto_layout (int* remaining_free_space_on_y, int rect_height) {
-	*remaining_free_space_on_y -= VERTICAL_PADDING;
-	int result = *remaining_free_space_on_y;
-
-	*remaining_free_space_on_y -= rect_height;
-	return result;
-}
-
-static void draw_slider (int width, int height, lc_color color, float max_value, float value, int* remaining_y_space, bool is_selected) {
+static void draw_slider (layout_info* layout, int width, int height, lc_color color, float max_value, float value, bool is_selected) {
 	// Calculate the slider rect
 	lc_rect rect = { };
-	rect.x = HORIZONTAL_PADDING;
-	rect.y = auto_layout (remaining_y_space, height);
 	rect.width = width;
 	rect.height = height;
+	layout_auto_position (layout, &rect);
 
 	// Draw the highlight if selected
 	if (is_selected) {
 		lc_color highlight_color = make_colorb (HIGHLIGHT_COLOR);
+
+		// Find a way to position the highlight via layout methods 
 		lc_rect highlight_rect = { };
 		highlight_rect.y = rect.y + VERTICAL_PADDING;	
 		highlight_rect.width = width + (2 * HORIZONTAL_PADDING);
@@ -169,6 +165,15 @@ static void draw_slider (int width, int height, lc_color color, float max_value,
 	handle_rect.y = rect.y;
 
 	opengl_rect (handle_rect, handle_color);
+}
+
+static void draw_color_swatch (layout_info* layout, int width, int height, lc_color color) {
+	lc_rect rect = { };
+	rect.width = width;
+	rect.height = height;
+	layout_auto_position (layout, &rect);
+
+	opengl_rect (rect, color);
 }
 
 void app_init (lc_memory* memory, platform_api platform, int client_width, int client_height) {
@@ -200,27 +205,33 @@ void app_update (lc_memory* memory, lc_input input) {
 
 	handle_input (app, input);
 
+	layout_info layout = { };
+	layout_set_client_dimensions (&layout, app -> client_width, app -> client_height);
+
 	lc_color clear_color = make_colorb (CLEAR_COLOR);
 	opengl_clear (app -> client_width, app -> client_height, clear_color);
-
-	int remaining_y_space = app -> client_height;
 
 	// (0,0) is bottom left, (width, height) is top right 
 	lc_color color = app -> current_color;
 	lc_rect color_rect = { };
 	color_rect.width = app -> client_width - (2 * HORIZONTAL_PADDING);
 	color_rect.height = app -> client_height / 3;
-	color_rect.x = HORIZONTAL_PADDING;
-	color_rect.y = auto_layout (&remaining_y_space, color_rect.height);
+	layout_auto_position (&layout, &color_rect);
 	opengl_rect (color_rect, color);
 
 	int slider_width = app -> client_width - (2 * HORIZONTAL_PADDING);
-	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (255, 0, 0), 1.0f, app -> current_color.r,
-				 &remaining_y_space, app -> current_component == CC_R);
-	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (0, 255, 0), 1.0f, app -> current_color.g,
-				 &remaining_y_space, app -> current_component == CC_G);
-	draw_slider (slider_width, SLIDER_HEIGHT, make_colorb (0, 0, 225), 1.0f, app -> current_color.b,
-				 &remaining_y_space, app -> current_component == CC_B);
+	draw_slider (&layout, slider_width, SLIDER_HEIGHT, make_colorb (255, 0, 0), 1.0f, app -> current_color.r,
+				 app -> current_component == CC_R);
+	draw_slider (&layout, slider_width, SLIDER_HEIGHT, make_colorb (0, 255, 0), 1.0f, app -> current_color.g,
+				 app -> current_component == CC_G);
+	draw_slider (&layout, slider_width, SLIDER_HEIGHT, make_colorb (0, 0, 225), 1.0f, app -> current_color.b,
+				 app -> current_component == CC_B);
+
+	layout_begin_horizontal_group (&layout); {
+		for (int i = 0; i < app -> color_swatches.count; ++i)
+			draw_color_swatch (&layout, SWATCH_WIDTH, SWATCH_HEIGHT, app -> color_swatches.colors[i]);
+	}
+	layout_end_horizontal_group (&layout, SWATCH_HEIGHT);
 }
 
 void app_close (lc_memory* memory) {
